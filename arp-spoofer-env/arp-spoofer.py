e8871715a2773a0700eef1007b83814c6f7a6b93
micklondonjr <micklondonjr@proton.me>
2024-08-31 23:37:34 +0700

    Added ability to run from command line

--- arp-spoofer-env/arp-spoofer.py
+++ arp-spoofer-env/arp-spoofer.py
@@ -8,7 +8,7 @@ import argparse
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast = broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
     if answered_list:
